0d8206893a57dd9211debf2e008fe96138372c57
Brian Cantoni <brian@cantoni.org>
2025-06-02 19:19:56 -0700

    Fix test execution

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,6 +1,7 @@
-from llm.plugins import pm
+from llm.plugins import load_plugins, pm
 
 
 def test_plugin_is_installed():
+    load_plugins()
     names = [mod.__name__ for mod in pm.get_plugins()]
     assert "llm_cmd" in names
